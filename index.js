f533f7b4338f8f43212d23efaa1e087a9b00d017
jsonic1337 <50801614+jsonic1337@users.noreply.github.com>
2020-03-22 11:33:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var VK = require("VK-Promise"), a = false,  b = false,
 vk = new VK("ТОКЕН");
-var sts = Array(1,2); // Айди стикеров
+var sts = Array(1, 2, 3); // Айди стикеров
 var name = ('олег'); //Имя
 var id = ('@jsonic'); //Пуш
 var vkl = ('твкл'); //Включить скрипт
@@ -13,13 +13,13 @@ vk.on('message', function onMessage(event, msg) {
     if(msg.body.toLowerCase() == vykl) {
       a = true;
     }
-if((msg.body.toLowerCase().includes(name) || msg.body.toLowerCase().includes(id)) && !a) {
-    var st = sts[Math.floor(Math.random()*sts.length)];
-    vk.messages.send({
-        peer_id: msg.peer_id,
-        sticker_id: st
-        });
-a = true; 
+    if((msg.body.toLowerCase().includes(name) || msg.body.toLowerCase().includes(id)) && !a) {
+      var st = sts[Math.floor(Math.random()*sts.length)];
+        vk.messages.send({
+          peer_id: msg.peer_id,
+          sticker_id: st
+         });
+          a = true; 
 setTimeout(() => {a = false;}, 30000); //Задержка в милисекундах
 }
     })
