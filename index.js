5cda01df458f1f7798a8a92458fa6284b0532141
jsonic1337 <50801614+jsonic1337@users.noreply.github.com>
2020-03-22 11:52:15 +0300

    fix

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ var sts = Array(1, 2, 3); // Айди стикеров
 var name = ('олег'); //Имя
 var id = ('@jsonic'); //Пуш
 var vkl = ('твкл'); //Включить скрипт
-var vykl = ('твыкл') //Выключить скрипт
+var vykl = ('твыкл'); //Выключить скрипт
+var time = 10
 vk.longpoll.start();
 vk.on('message', function onMessage(event, msg) {
-    if(msg.body.toLowerCase() == vkl) {
-      a = false;
-    }
-    if(msg.body.toLowerCase() == vykl) {
-      a = true;
-    }
     if((msg.body.toLowerCase().includes(name) || msg.body.toLowerCase().includes(id)) && !a) {
       var st = sts[Math.floor(Math.random()*sts.length)];
         vk.messages.send({
           peer_id: msg.peer_id,
           sticker_id: st
-         });
+          });
           a = true; 
-setTimeout(() => {a = false;}, 30000); //Задержка в милисекундах
+setTimeout(() => {a = false;}, time); //Задержка в милисекундах
+}
+if(!msg.out) return;
+    if(msg.body.toLowerCase() == vkl) {
+      a = false;
+}
+    if(msg.body.toLowerCase() == vykl) {
+      a = true;
 }
     })
 
