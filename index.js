0f4a0fae74a83ad5ae8ffef9d1b14041d6d402ff
jsonic1337 <50801614+jsonic1337@users.noreply.github.com>
2020-03-22 11:54:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var VK = require("VK-Promise"), a = false,  b = false,
 vk = new VK("ТОКЕН");
-var sts = Array(1, 2, 3); // Айди стикеров
+var sts = Array(1, 2, 3); //Айди стикеров
 var name = ('олег'); //Имя
 var id = ('@jsonic'); //Пуш
 var vkl = ('твкл'); //Включить скрипт
 var vykl = ('твыкл'); //Выключить скрипт
-var time = 10 //Задержка в милисекундах
+var time = 10 //Кулдаун в милисекундах
 vk.longpoll.start();
 vk.on('message', function onMessage(event, msg) {
     if((msg.body.toLowerCase().includes(name) || msg.body.toLowerCase().includes(id)) && !a) {
