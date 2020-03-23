28e3e1773aa81592fc9183f218e4b68731bed5e2
jsonic1337 <50801614+jsonic1337@users.noreply.github.com>
2020-03-23 10:14:29 +0300

    Возможность добавлять чаты в вайт лист по команде

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var VK = require("VK-Promise"), a = false,  b = false,
 vk = new VK("ТОКЕН");
-var sts = Array(1, 2, 3); //Айди стикеров
+var sts = [1, 2, 3]; //Айди стикеров
 var name = ('олег'); //Имя
 var id = ('@jsonic'); //Пуш
 var vkl = ('твкл'); //Включить скрипт
 var vykl = ('твыкл'); //Выключить скрипт
-var time = 10000 //Задержка в милисекундах, 1 секунда = 1000 милисекунд
+var chats = [];
+var time = 1000 //Задержка в милисекундах, 1 секунда = 1000 милисекунд
+
 vk.longpoll.start();
 vk.on('message', function onMessage(event, msg) {
-    if((msg.body.toLowerCase().includes(name) || msg.body.toLowerCase().includes(id)) && !b && !a) {
+    if((msg.body.toLowerCase().includes(name) || msg.body.toLowerCase().includes(id)) && chats.includes(msg.chat_id) && !b && !a) {
       var st = sts[Math.floor(Math.random()*sts.length)];
         vk.messages.send({
           peer_id: msg.peer_id,
@@ -34,5 +36,21 @@ setTimeout(() => {a = false;}, time);
     })
       msg.send('Триггеры выключены!');
 }
+if(msg.body.toLowerCase() == "!вайт") {
+    chats.push(msg.chat_id);
+    vk.messages.delete({
+      delete_for_all: 1,
+      message_ids: msg.id.toString()
+    })
+      msg.send('Чат добавлен в вайт лист');
+      }
+      if((msg.body.toLowerCase() == "!двайт") && (chats.indexOf(msg.chat_id) != -1)) {
+    delete chats[chats.indexOf(msg.chat_id)];
+    vk.messages.delete({
+      delete_for_all: 1,
+      message_ids: msg.id.toString()
+    })
+      msg.send('Чат убран из вайт листа');
+      }
     })
     
