7b00f602f939ae01ad3ecbcb63a9e08d0765384c
jsonic1337 <50801614+jsonic1337@users.noreply.github.com>
2020-03-23 14:56:55 +0300

    Флит молодец

--- index.js
+++ index.js
@@ -1,76 +1,68 @@
-var VK = require("VK-Promise"), a = false,  b = false,
-vk = new VK("ТОКЕН");
-var name = ('олег'); //Имя
-var id = ('@jsonic'); //Айди
-var time = 40000; //Задержка в милисекундах, 1 секунда = 1000 милисекунд
-var add = ('!стикер');
-var del = ('!убрать');
-var sts = [1, 2, 3];
-var chats = []
+let VK = require("VK-Promise"),
+    vk = new VK(""); //Токен
+
+var name = ('олег'),  //Имя
+    id = ('@jsonic'); //Айди
+
+var time = 10; //Задержка в секундах
+
+let add = ('!стикер'),
+    del = ('!убрать'),
+    kd = ('!кд');
+
+var sts = [], chats = [],
+    a = false, b = false;
+
 vk.longpoll.start();
- vk.on('message', function onMessage(event, msg) {
-    if((msg.body.toLowerCase().includes(name) || msg.body.toLowerCase().includes(id)) && chats.includes(msg.chat_id) && !b && !a) {
-        var st = sts[Math.floor(Math.random()*sts.length)];
-         vk.messages.send({
-            peer_id: msg.peer_id,
-            sticker_id: st
-            });
-            a = true; 
-            setTimeout(() => {a = false;}, time); 
-           }
- if(!msg.out) return
-      if(msg.body.toLowerCase() == 'твкл') {
-        b = false;
-         vk.messages.edit({
-            peer_id: msg.peer_id,
-            message: 'Триггеры включены!',
-            message_id: msg.id.toString()
-            })
-           }
-      if(msg.body.toLowerCase() == 'твыкл') {
-        b = true;
-         vk.messages.edit({
-            peer_id: msg.peer_id,
-            message: 'Триггеры выключены!',
-            message_id: msg.id.toString()
-            })
-           }
-      if((msg.body.toLowerCase() == '!вайт') && (chats.indexOf(msg.chat_id) == -1)) {
-        chats.push(msg.chat_id);
-         vk.messages.edit({
-            peer_id: msg.peer_id,
-            message: 'Чат добавлен в вайт лист',
-            message_id: msg.id.toString()
-            })
-           }
-      if((msg.body.toLowerCase() == '!двайт') && (chats.indexOf(msg.chat_id) != -1)) {
-        delete chats[chats.indexOf(msg.chat_id)];
-         vk.messages.edit({
-            peer_id: msg.peer_id,
-            message: 'Чат убран из вайт листа',
-            message_id: msg.id.toString()
-            })
-           }
-      if(msg.body.toLowerCase().startsWith(add)) {
-        let num = msg.body.toLowerCase().replace(add, '').trim();
-         if (sts.indexOf(num) == -1) {
-         sts.push(num);
-          vk.messages.edit({
-             peer_id: msg.peer_id,
-             message: 'Стикер добавлен',
-             message_id: msg.id.toString()
-             })
-            }
-           }
-      if(msg.body.toLowerCase().startsWith(del)) {
-        let num = msg.body.toLowerCase().replace(del, '').trim();
-         if (sts.indexOf(num) != -1) {
-         delete sts[sts.indexOf(num)];
-          vk.messages.edit({
-             peer_id: msg.peer_id,
-             message: 'Стикер удален',
-             message_id: msg.id.toString()
-             })
-            }
-           }
-    })
+
+vk.on('message', function onMessage(event, msg) {
+
+  if((msg.body.toLowerCase().includes(name) || msg.body.toLowerCase().includes(id)) && chats.includes(msg.chat_id) && !b && !a) {
+      var st = sts[Math.floor(Math.random()*sts.length)];
+
+      vk.messages.send({ peer_id: msg.peer_id, sticker_id: st });
+
+      a = true;
+      setTimeout(() => { a = false; }, time * 1000);
+  }
+
+  if(!msg.out) return
+
+  if(msg.body.toLowerCase() == 'твкл') {
+      b = false;
+
+      vk.messages.edit({ peer_id: msg.peer_id, message: 'Триггеры включены!', message_id: msg.id.toString() });
+  } else if(msg.body.toLowerCase() == 'твыкл') {
+      b = true;
+
+      vk.messages.edit({ peer_id: msg.peer_id, message: 'Триггеры выключены!', message_id: msg.id.toString() });
+  } else if((msg.body.toLowerCase() == '!вайт') && (!chats.includes(msg.chat_id))) {
+      chats.push(msg.chat_id);
+
+      vk.messages.edit({ peer_id: msg.peer_id, message: 'Чат добавлен в вайт лист!', message_id: msg.id.toString() });
+  } else if((msg.body.toLowerCase() == '!двайт') && (chats.includes(msg.chat_id))) {
+      delete chats[chats.indexOf(msg.chat_id)];
+
+      vk.messages.edit({ peer_id: msg.peer_id, message: 'Чат убран из вайт листа!', message_id: msg.id.toString() });
+  } else if(msg.body.toLowerCase().startsWith(add)) {
+      let num = msg.body.toLowerCase().replace(add, '').trim();
+
+      if (!sts.includes(num)) {
+          sts.push(num);
+
+          vk.messages.edit({ peer_id: msg.peer_id, message: 'Стикер добавлен!', message_id: msg.id.toString() });
+      }
+  } else if(msg.body.toLowerCase().startsWith(del)) {
+      let num = msg.body.toLowerCase().replace(del, '').trim();
+
+      if (!sts.includes(num)) {
+          delete sts[sts.indexOf(num)];
+
+          vk.messages.edit({ peer_id: msg.peer_id, message: 'Стикер удален!', message_id: msg.id.toString() });
+      }
+  } else if(msg.body.toLowerCase().startsWith(kd)) {
+      time = parseInt(msg.body.toLowerCase().replace(kd, '').trim());
+
+      vk.messages.edit({ peer_id: msg.peer_id, message: ('Кд изменено на ' + time + ' секунд'), message_id: msg.id.toString() });
+  }
+})
